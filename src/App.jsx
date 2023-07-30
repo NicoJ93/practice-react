c72291f7bff8665fc7890ed5c56b50a214fd2849
NicoJ93 <lucasnjuarez@gmail.com>
2023-07-30 17:41:22 -0300

    First commit

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+export function App() {
+    return (
+        <article style={{display: 'flex', color: '#fff'}}>
+            <header>
+                <img alt='Avatar de canal recomendado' src='https://unavatar.io/twitter/kikobeats'/>
+                <div>
+                    <strong>Todo sobre River</strong>
+                    <span>@TodoRiver</span>
+                </div>
+            </header>
+            <aside>
+                <button>
+                    Unirse
+                </button>
+            </aside>
+        </article>
+        
+    )
+}
