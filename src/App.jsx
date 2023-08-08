f6659a84a6e70dc9d369d8208eca5d53246aa842
NicoJ93 <lucasnjuarez@gmail.com>
2023-08-07 21:33:33 -0300

    Agregando estilos

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,16 @@
+import './App.css'
+
 export function App() {
     return (
-        <article style={{display: 'flex', color: '#fff'}}>
-            <header>
-                <img alt='Avatar de canal recomendado' src='https://unavatar.io/twitter/kikobeats'/>
-                <div>
+        <article className='ds-profileCard'>
+            <header className='ds-profileCard-header'>
+                <img 
+                className='ds-followCard-avatar' 
+                alt='Avatar de canal recomendado'
+                src='https://unavatar.io/twitter/kikobeats'/>
+                <div className='ds-followCard-info'>
                     <strong>Todo sobre River</strong>
-                    <span>@TodoRiver</span>
+                    <span className='ds-followCard-infoUsername'>@TodoRiver</span>
                 </div>
             </header>
             <aside>
